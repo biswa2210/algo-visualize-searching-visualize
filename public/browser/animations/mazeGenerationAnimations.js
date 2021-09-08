d459f70653a903311669efd953ae1132de1b8beb
Biswarup Bhattacharjee <bbiswa471@gmail.com>
2021-09-09 01:32:07 +0530

    EDITED BY BISWA

---
+++ public/browser/animations/mazeGenerationAnimations.js
@@ -0,0 +1,28 @@
+function mazeGenerationAnimations(board) {
+  let nodes = board.wallsToAnimate.slice(0);
+  let speed = board.speed === "fast" ?
+    5 : board.speed === "average" ?
+      25 : 75;
+  function timeout(index) {
+    setTimeout(function () {
+        if (index === nodes.length){
+          board.wallsToAnimate = [];
+          board.toggleButtons();
+          return;
+        }
+        nodes[index].className = board.nodes[nodes[index].id].weight === 15 ? "unvisited weight" : "wall";
+        timeout(index + 1);
+    }, speed);
+  }
+
+  timeout(0);
+};
+
+module.exports = mazeGenerationAnimations;
+
+/*
+    *****************
+    DONE BY:-  BISWARUP BHATTACHARJEE
+    E-MAIL:-    bbiswa471@gmail.com 
+    *****************
+*/
